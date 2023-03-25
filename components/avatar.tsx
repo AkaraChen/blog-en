f8a946792c3e81378ff03451a70ffe4adb13e628
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-03-25 19:48:35 +0800

    style: add prettier

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Image from 'next/image'
 
 type Props = {
   name: string
@@ -8,7 +8,13 @@ type Props = {
 const Avatar = ({ name, picture }: Props) => {
   return (
     <div className="flex items-center">
-      <Image src={picture} width={48} height={48} className="w-12 h-12 rounded-full mr-4" alt={name} />
+      <Image
+        src={picture}
+        width={48}
+        height={48}
+        className="w-12 h-12 rounded-full mr-4"
+        alt={name}
+      />
       <div className="text-xl font-bold">{name}</div>
     </div>
   )
