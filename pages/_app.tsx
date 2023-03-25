b04000631ac9e5c9d99c2559aafd1bc863a718fc
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-03-25 21:05:39 +0800

    feat: add progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import { AppProps } from 'next/app'
 import '~/styles/index.css'
+import { ThemeProvider } from 'next-themes'
+import { useProgress } from '@akrc/next-progress'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useProgress()
+  return (
+    <ThemeProvider attribute="class" enableSystem>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
